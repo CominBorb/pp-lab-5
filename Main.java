7f715d8c7d36d6a87392b0d2ad0e22442996923f
CominBorb <norbert.oledzki@gmail.com>
2024-05-04 12:05:41 +0200

    ok

--- Main.java
+++ Main.java
@@ -1,17 +1,8 @@
-import company.beans.Person;
-import company.exception.InvalidAgeException;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Tworzenie obiektu Person
-            Person person = new Person("Maciek", 40);
-
-            // Wypisywanie danych osoby
-            System.out.println("Imię: " + person.getName());
-            System.out.println("Wiek: " + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Siema eniu");
     }
 }
