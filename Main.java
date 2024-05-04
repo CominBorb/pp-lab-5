1743a6d26284c0bd5c4f3ba96080bb680389f147
CominBorb <166212995+CominBorb@users.noreply.github.com>
2024-05-04 11:41:10 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,8 @@ import company.exception.InvalidAgeException;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Tworzenie obiektu Person
             Person person = new Person("Maciek", 40);
 
-            // Wypisywanie danych osoby
             System.out.println("Imię: " + person.getName());
             System.out.println("Wiek: " + person.getAge());
         } catch (InvalidAgeException e) {
